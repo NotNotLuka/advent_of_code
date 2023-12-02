cef4b01e2fb424ff0658cb5ea7eefc4341f473c4
NotNotLuka <papez.luka@gmail.com>
2023-12-02 19:10:49 +0100

    indent fix

--- src/solutions/days/day_2.ml
+++ src/solutions/days/day_2.ml
@@ -47,31 +47,31 @@ module Solver : Solver = struct
     let all_lines = (split '\n' data) in
     string_of_int (game_info all_lines 0)
 
-    let rec check_each_minimum data minimum_cubes =
-      match data with
-      | [] -> minimum_cubes
-      | h :: t -> let colour = split ' ' (String.trim h) in 
-                  let n = int_of_string (List.nth colour 0) in 
-                  match String.trim (List.nth colour 1) with
-                  | "red" -> if n <= minimum_cubes.red 
+  let rec check_each_minimum data minimum_cubes =
+    match data with
+    | [] -> minimum_cubes
+    | h :: t -> let colour = split ' ' (String.trim h) in 
+                let n = int_of_string (List.nth colour 0) in 
+                match String.trim (List.nth colour 1) with
+                | "red" -> if n <= minimum_cubes.red 
+                          then check_each_minimum t minimum_cubes 
+                          else check_each_minimum t {minimum_cubes with red=n}
+                | "blue" -> if n <= minimum_cubes.blue 
+                          then check_each_minimum t minimum_cubes 
+                          else check_each_minimum t {minimum_cubes with blue=n}
+                | "green" -> if n <= minimum_cubes.green 
                             then check_each_minimum t minimum_cubes 
-                            else check_each_minimum t {minimum_cubes with red=n}
-                  | "blue" -> if n <= minimum_cubes.blue 
-                            then check_each_minimum t minimum_cubes 
-                            else check_each_minimum t {minimum_cubes with blue=n}
-                  | "green" -> if n <= minimum_cubes.green 
-                              then check_each_minimum t minimum_cubes 
-                              else check_each_minimum t {minimum_cubes with green=n}
-                  | s -> failwith ("unknown colour " ^ s) 
-    let rec get_product data acc =
-      match data with
-      | [] -> acc.red * acc.blue * acc.green
-      | h :: t -> get_product t (check_each_minimum (split ',' h) acc)
-    let rec get_sum lines acc =
-      match lines with
-      | [] -> acc
-      | h :: t -> let info = split ':' h in 
-                  get_sum t ((get_product (split ';' (List.nth info 1)) {red=0;blue=0;green=0}) + acc)
+                            else check_each_minimum t {minimum_cubes with green=n}
+                | s -> failwith ("unknown colour " ^ s) 
+  let rec get_product data acc =
+    match data with
+    | [] -> acc.red * acc.blue * acc.green
+    | h :: t -> get_product t (check_each_minimum (split ',' h) acc)
+  let rec get_sum lines acc =
+    match lines with
+    | [] -> acc
+    | h :: t -> let info = split ':' h in 
+                get_sum t ((get_product (split ';' (List.nth info 1)) {red=0;blue=0;green=0}) + acc)
   let task2 data _part1 =
 
     let all_lines = (split '\n' data) in
